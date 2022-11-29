a73cb6614da0a30b5c3cd5cac9e2b6bf578ab433
Samuel <samuelemeka2021@outlook.com>
2022-11-29 18:18:14 +0100

    added carousel component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
-  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
-
+  styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent  {
 
-  images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-  constructor(config: NgbCarouselConfig) {
-    //
-    config.interval = 2000;
-    config.keyboard = true;
-    config.pauseOnHover = true;
+  constructor() {
   }
 
 }
